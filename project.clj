ead01c62a2d45e97ffa9f72da711ce2339446fba
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-06-16 13:39:50 +0000

    up deps

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Flexible select-style component for use in Om apps"
   :url "https://github.com/condense/select-om-all"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3291"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.omcljs/om "0.8.8"]
                  [sablono "0.3.4"]
-                 [cljsjs/fixed-data-table "0.2.0-0"]
+                 [cljsjs/fixed-data-table "0.3.0-0"]
                  [figwheel "0.3.3" :scope "test"] ]
 
   :plugins [[lein-cljsbuild "1.0.6"]
